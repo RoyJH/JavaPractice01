35b222077d99298d03f95690b246de146dada122
zithlos <zithlos@localhost>
2020-01-13 20:36:41 -0600

    Getters and Setters

---
+++ src/GettingAndSetting.java
@@ -0,0 +1,11 @@
+public class GettingAndSetting {
+	private int number;
+	
+	public int getNumber(){
+		return this.number;
+	}
+	
+	public void setNumber(int num){
+		this.number = num;
+	}	
+}
---
+++ src/JavaMain.java
@@ -0,0 +1,12 @@
+
+public class JavaMain {
+
+	public static void main(String[] args) {
+		GettingAndSetting obj = new GettingAndSetting();
+		obj.setNumber(15);
+		int num = obj.getNumber();
+		
+		System.out.println(num);
+	}
+
+}
